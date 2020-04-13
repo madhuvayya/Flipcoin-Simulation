d952369e83dcfc33366d0575915e6fcdd740703c
madhuvayya <madhu.vayya@gmail.com>
2020-04-13 22:56:40 +0530

    Storing the values into a dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-random=$((RANDOM%2))
+echo "enter a number to flip coin:"
+read n
 
-if [ $random -eq 1 ]
-then
-	echo Head
-else
-	echo Tail
-fi
+declare -A outComes
+
+for((i=0;i<n;i++))
+do
+	random=$((RANDOM%2))
+
+	if [ $random -eq 1 ]
+	then
+		outComes[$i]="H"
+	else
+		outComes[$i]="T"
+	fi
+done
+
+echo ${outComes[@]}
+echo ${!outComes[@]}

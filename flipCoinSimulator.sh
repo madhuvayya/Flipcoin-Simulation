f14db35da90d5824de3c74572e5a1e47646b2d26
madhuvayya <madhu.vayya@gmail.com>
2020-04-14 10:01:59 +0530

    Added case statement to check the flipping doublet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,21 +1,89 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "enter a number to flip coin:"
 read n
 
-declare -A outComes
+singletPossible=("H" "T")
+doubletPossible=("HH" "HT" "TH" "TT")
+
+declare -A singlet
+declare -A doublet
 
 for((i=0;i<n;i++))
 do
-	random=$((RANDOM%2))
+	singletCheck=$((RANDOM%2))
 
-	if [ $random -eq 1 ]
+	if [ $singletCheck -eq 1 ]
 	then
-		outComes[$i]="H"
+		singlet[$i]="H"
 	else
-		outComes[$i]="T"
+		singlet[$i]="T"
+	fi
+
+	doubletCheck=$((RANDOM%4))
+	case $doubletCheck in
+		0 )
+			doublet[$i]="HH"
+			;;
+		1 )
+			doublet[$i]="HT"
+			;;
+		2 )
+			doublet[$i]="TH"
+			;;
+		3 )
+			doublet[$i]="TT"
+			;;
+	esac
+
+done
+
+echo ${singlet[@]}
+echo ${!singlet[@]}
+
+echo ${doublet[@]}
+echo ${!doublet[@]}
+
+k=0
+for i in ${singletPossible[@]}
+do
+	t=0
+	for j in ${singlet[@]}
+	do
+		if [ $i = $j ]
+		then
+			((t++));
+		fi
+	done
+
+	if [ $t -gt $k ]
+	then
+		max=$i
+		k=$t
 	fi
 done
 
-echo ${outComes[@]}
-echo ${!outComes[@]}
+per=$(($k / $n * 100))
+echo "Percentage: $per"
+
+k=0
+for i in ${doubletPossible[@]}
+do
+	t=0
+	for j in ${doublet[@]}
+	do
+		if [ $i = $j ]
+		then
+			((t++));
+		fi
+	done
+
+	if [ $t -gt $k ]
+	then
+		max=$i
+		k=$t
+	fi
+done
+
+per=$(($k / $n * 100))
+echo "Percentage: $per"

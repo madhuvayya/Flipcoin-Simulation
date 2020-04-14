19748e35f428f056c4552aeedf7b6e15c9ca42af
madhuvayya <madhu.vayya@gmail.com>
2020-04-14 10:31:10 +0530

    Added case statement to get triplet's

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "enter a number to flip coin:"
 read n
 
 singletPossible=("H" "T")
 doubletPossible=("HH" "HT" "TH" "TT")
+tripletPossible=("HHH" "HHT" "HTH" "HTT" "THH" "THT" "TTH" "TTT")
 
 declare -A singlet
 declare -A doublet
+declare -A triplet
 
 for((i=0;i<n;i++))
 do
@@ -36,13 +38,37 @@ do
 			;;
 	esac
 
+	tripletCheck=$((RANDOM%8))
+	case $doubletCheck in
+		0 )
+			triplet[$i]="HHH"
+			;;
+		1 )
+			triplet[$i]="HHT"
+			;;
+		2 )
+			triplet[$i]="HTH"
+			;;
+		3 )
+			triplet[$i]="HTT"
+			;;
+		4 )
+			triplet[$i]="THH"
+			;;
+		5 )
+			triplet[$i]="THT"
+			;;
+		6 )
+			triplet[$i]="TTH"
+			;;
+		7 )
+			triplet[$i]="TTT"
+			;;
+	esac
 done
 
 echo ${singlet[@]}
 echo ${!singlet[@]}
-
-echo ${doublet[@]}
-echo ${!doublet[@]}
 
 k=0
 for i in ${singletPossible[@]}
@@ -64,7 +90,10 @@ do
 done
 
 per=$(($k / $n * 100))
-echo "Percentage: $per"
+echo "Singlet percentage: $per"
+
+echo ${doublet[@]}
+echo ${!doublet[@]}
 
 k=0
 for i in ${doubletPossible[@]}
@@ -86,4 +115,29 @@ do
 done
 
 per=$(($k / $n * 100))
-echo "Percentage: $per"
+echo "Doublet percentage: $per"
+
+echo ${triplet[@]}
+echo ${!triplet[@]}
+
+k=0
+for i in ${tripletPossible[@]}
+do
+	t=0
+	for j in ${triplet[@]}
+	do
+		if [ $i = $j ]
+		then
+			((t++));
+		fi
+	done
+
+	if [ $t -gt $k ]
+	then
+		max=$i
+		k=$t
+	fi
+done
+
+per=$(($k / $n * 100))
+echo "Triplet percentage: $per"
